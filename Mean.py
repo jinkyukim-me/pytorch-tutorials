60bc4724aab0fb572427c3c346074dbd2bd7d4e4
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 02:31:11 +0900

    Update Mean.py

--- Mean.py
+++ Mean.py
@@ -1,3 +1,17 @@
 import torch
-t = torch.FloatTensor([1,2])
+t = torch.FloatTensor([1,2]) # 32-bit floating point CPU tensor
+t1 = torch.DoubleTensor([1,2]) # 64-bit floating point CPU tensor
 print('t.mean() = ', t.mean())
+print('t1.mean() = ', t1.mean())
+
+# Can't use mean() on integers
+t = torch.IntTensor([1,2]) # 32-bit Inter(signed) CPU tensor
+t1 = torch.LongTensor([1,2]) # 64-bit Inter(signed) CPU tensor
+try:
+    print('t.mean() = ', t.mean())
+except Exception as E:
+    print(E)
+try:
+    print('t1.mean() = ', t1.mean())
+except Exception as E:
+    print(E)
