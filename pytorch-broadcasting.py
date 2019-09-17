efbd27e63b8e951e474728f550ded0f13ab05e9c
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 00:15:43 +0900

    벡터와 스칼라의 합 추가

--- pytorch-broadcasting.py
+++ pytorch-broadcasting.py
@@ -1,8 +1,17 @@
 import torch
-# same shape
+# Same Shape
 m1 = torch.FloatTensor([[3, 2]])
 m2 = torch.FloatTensor([[2, 3]])
-print('same shape')
+print('Same Shape')
 print('Size of m1 = ', m1.shape)
 print('Size of m2 = ', m2.shape)
 print('m1 + m2 = ', m1+m2)
+print()
+# Vector + Scalar
+m1 = torch.FloatTensor([[2,3]])
+m2 = torch.FloatTensor([3]) # 3 -> [[3, 3]]
+print('Vector + Scalar')
+print('Size of m1 = ', m1.shape)
+print('Size of m2 = ', m2.shape)
+print('m1 + m2 = ', m1+m2)
+print()
