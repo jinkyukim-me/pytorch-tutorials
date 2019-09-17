298973268da8e6872876e930e2fde7a42c865ac3
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-17 23:21:17 +0900

    Add 1D Array with Pytorch

--- numpy-tutorial1.py
+++ numpy-tutorial1.py
@@ -21,3 +21,17 @@ print(t)
 
 print('Rank of t: ', t.ndim) # 차원 출력
 print('Shape of t: ', t.shape) # 크기 출력
+print()
+
+# 1D Array with PyTorch
+t = torch.FloatTensor([0., 1., 2., 3., 4., 5., 6.])
+print('1D Array with Pytorch')
+print(t)
+
+print('Rank of t: ', t.dim()) # 차원 출력
+print('Shape of t: ', t.shape) # 크기 출력
+print('Size of t: ', t.size()) # 크기 출력
+
+print('t[0] t[1] t[-1] = ', t[0], t[1], t[-1]) # Element
+print('t[2:5] t[4:-1] = ', t[2:5], t[4:-1]) # Slicing
+print('t[:2] t[3:] = ', t[:2], t[3:]) # Slicing
