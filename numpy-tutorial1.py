57f94e4f853138ef3a486d076154493ecf520212
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-17 23:42:30 +0900

    Add 2D Array with PyTorch

--- numpy-tutorial1.py
+++ numpy-tutorial1.py
@@ -21,6 +21,9 @@ print(t)
 
 print('Rank of t: ', t.ndim) # 차원 출력
 print('Shape of t: ', t.shape) # 크기 출력
+print('t[:, 1] = ', t[:, 1])
+print('Shape of t[:, 1] = ' ,t[:, 1].shape)
+print('t[:, :-1] = \n', t[:, :-1])
 print()
 
 # 1D Array with PyTorch
@@ -35,3 +38,19 @@ print('Size of t: ', t.size()) # 크기 출력
 print('t[0] t[1] t[-1] = ', t[0], t[1], t[-1]) # Element
 print('t[2:5] t[4:-1] = ', t[2:5], t[4:-1]) # Slicing
 print('t[:2] t[3:] = ', t[:2], t[3:]) # Slicing
+print()
+
+# 2D Array with PyTorch
+t = torch.FloatTensor([[1., 2., 3.],
+                       [4., 5., 6.],
+                       [7., 8., 9.],
+                       [10., 11., 12.]])
+print('2D Array with Pytorch')
+print(t)
+
+print('Rank of t: ', t.dim()) # 차원 출력
+print('Shape of t: ', t.shape) # 크기 출력
+print('Size of t: ', t.size()) # 크기 출력
+print('t[:, 1] = ', t[:, 1])
+print('Size of t[:, 1] = ' ,t[:, 1].size())
+print('t[:, :-1] = \n',t[:, :-1]) # :-1에서 슬라이싱할 때 -1은 포함되지 않음에 주의
