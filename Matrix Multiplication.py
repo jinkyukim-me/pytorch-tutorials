575ca72bae2d40a869408f0a5d945c3167f20ef4
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 01:17:12 +0900

    Multiplication vs Matrix Multiplication 추가

--- Matrix Multiplication.py
+++ Matrix Multiplication.py
@@ -1,1 +1,11 @@
 import torch
+print('Multiplication vs Matrix Multiplication')
+m1 = torch.FloatTensor([[1,2],[3,4]])
+m2 = torch.FloatTensor([[1],[2]])
+print('m1 = \n', m1)
+print('m2 = \n', m2)
+print('Shape of Matrix 1 = ', m1.shape) # 2 x 2
+print('Shape of Matrix 2 = ', m2.shape) # 2 x 1
+print('m1.matmul(m2) = \n', m1.matmul(m2))
+print('m1 * m2 = \n', m1 * m2)
+print('m1.mul(m2) = \n', m1.mul(m2))
