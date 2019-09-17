bfb7c5d2bed3a0b6d73d81653dbf7782716b51be
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 04:02:52 +0900

    Update Max and Argmax.py

--- Max and Argmax.py
+++ Max and Argmax.py
@@ -1,1 +1,14 @@
 import torch
+
+t = torch.FloatTensor([[1,2],[3,4]])
+print('t = \n', t)
+print()
+print('t.max() = ', t.max())
+print()
+print('t.max(dim=0) = \n', t.max(dim=0))
+print('Max = ', t.max(dim=0)[0])
+print('Argmax = ', t.max(dim=0)[1])
+print()
+print('t.max(dim=1) = \n', t.max(dim=1))
+print('Max = ', t.max(dim=1)[0])
+print('Argmax = ', t.max(dim=1)[1])
