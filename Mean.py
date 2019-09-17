11246508d5ff048461bcdfb4f7ddedbc1b05b493
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 03:34:46 +0900

    torch.mean() 추가

--- Mean.py
+++ Mean.py
@@ -15,10 +15,23 @@ try:
     print('t1.mean() = ', t1.mean())
 except Exception as E:
     print(E)
-
+print()
 t = torch.FloatTensor([[1,2],[3,4]])
 print('t = \n', t)
 print('t.mean() = ', t.mean())
 print('t.mean(dim=0) = ', t.mean(dim=0))
 print('t.mean(dim=1) = ', t.mean(dim=1))
 print('t.mean(dim=-1) = ', t.mean(dim=-1))
+print()
+t = torch.FloatTensor([[1,2,3,4],[5,6,7,8],[5,4,3,2],[9,7,5,3]])
+print('t = \n', t)
+print('torch.mean(input, dim, keepdim=False, out=None) → Tensor')
+print('torch.mean(t) = ', torch.mean(t))
+print('torch.mean(t,0) = ', torch.mean(t,0))
+print('torch.mean(t,1) = ', torch.mean(t,1))
+print('torch.mean(t,0,True) = ', torch.mean(t,0,True))
+print('torch.mean(t,1,True) = \n', torch.mean(t,1,True))
+try:
+    print('torch.mean(t,2) = ', torch.mean(t,2))
+except Exception as E:
+    print(E)
