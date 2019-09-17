703eebc5d78210a339b4e70399a4acc422c04c2a
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 00:09:28 +0900

    같은 사이즈인 경우

--- pytorch-broadcasting.py
+++ pytorch-broadcasting.py
@@ -1,1 +1,8 @@
 import torch
+# same shape
+m1 = torch.FloatTensor([[3, 2]])
+m2 = torch.FloatTensor([[2, 3]])
+print('same shape')
+print('Size of m1 = ', m1.shape)
+print('Size of m2 = ', m2.shape)
+print('m1 + m2 = ', m1+m2)
