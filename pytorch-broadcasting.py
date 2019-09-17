2882eb51f603be04554f4d56db2327383d31a01e
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 00:24:56 +0900

    1 x 2 Vector + 2 x 1 Vector 추가

--- pytorch-broadcasting.py
+++ pytorch-broadcasting.py
@@ -3,6 +3,8 @@ import torch
 m1 = torch.FloatTensor([[3, 2]])
 m2 = torch.FloatTensor([[2, 3]])
 print('Same Shape')
+print('m1 = ', m1)
+print('m2 = ', m2)
 print('Size of m1 = ', m1.shape)
 print('Size of m2 = ', m2.shape)
 print('m1 + m2 = ', m1+m2)
@@ -11,7 +13,19 @@ print()
 m1 = torch.FloatTensor([[2,3]])
 m2 = torch.FloatTensor([3]) # 3 -> [[3, 3]]
 print('Vector + Scalar')
+print('m1 = ', m1)
+print('m2 = ', m2)
 print('Size of m1 = ', m1.shape)
 print('Size of m2 = ', m2.shape)
 print('m1 + m2 = ', m1+m2)
+print()
+# 1 x 2 Vector + 2 x 1 Vector
+m1 = torch.FloatTensor([[2,3]]) # [[2,3]] -> [[2,3],[2,3]]
+m2 = torch.FloatTensor([[3],[4]]) # [[3],[4]] -> [[3,3],[4,4]]
+print('1 x 2 Vector + 2 x 1 Vector')
+print('m1 = ', m1)
+print('m2 = \n', m2)
+print('Size of m1 = ', m1.shape)
+print('Size of m2 = ', m2.shape)
+print('m1 + m2 = \n', m1+m2)
 print()
