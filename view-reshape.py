f40ecabe393479bbc050f1b2d83edfd41f4c5608
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 13:11:36 +0900

    Update view-reshape.py

--- view-reshape.py
+++ view-reshape.py
@@ -1,1 +1,11 @@
 import torch
+import numpy as np
+t = np.array([[[0,1,2],[3,4,5]], [[6,7,8],[9,10,11]]])
+print('t = \n', t)
+print('t.shape = ', t.shape)
+ft = torch.FloatTensor(t)
+print('ft = \n', ft)
+print('ft.shape = ', ft.shape)
+print()
+print('View')
+print('ft.view([-1, 3]) = \n', ft.view([-1, 3]))
