27c51983062071701e7f35f66fd681886d4228d8
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 17:30:49 +0900

    view 추가

--- Unsqueeze.py
+++ Unsqueeze.py
@@ -10,4 +10,10 @@ print('ft.unsqueeze(-2) = ', ft.unsqueeze(-2))
 print('ft.unsqueeze(-2).shape = ', ft.unsqueeze(-2).shape)
 print('ft.unsqueeze(-1) = \n', ft.unsqueeze(-1))
 print('ft.unsqueeze(-1).shape = ', ft.unsqueeze(-1).shape)
+print('view')
+print('ft.view(1, -1) = ', ft.view(1, -1))
+print('ft.view(1, -1).shape = ', ft.view(1, -1).shape)
+print('ft.view(-1, 1) = \n', ft.view(-1, 1))
+print('ft.view(-1, 1).shape = ', ft.view(-1, 1).shape)
+
 
