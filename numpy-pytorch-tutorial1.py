13e3814832b934b7047b70ccf81c872f3b96f2a2
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-18 14:49:50 +0900

    3d array with numpy 추가

--- numpy-pytorch-tutorial1.py
+++ numpy-pytorch-tutorial1.py
@@ -1,14 +1,18 @@
 import numpy as np
 import torch
-
+# 0D Array with Numpy
+t = np.array(5)
+print('0D Array with Numpy')
+print('t = ', t)
+print('Rank of t: ', t.ndim) # 차원 출력
+print('Shape of t: ', t.shape) # 크기 출력
+print()
 # 1D Array with NumPy
 t = np.array([0., 1., 2., 3., 4., 5., 6.])
 print('1D Array with NumPy')
-print(t)
-
+print('t = ', t)
 print('Rank of t: ', t.ndim) # 차원 출력
 print('Shape of t: ', t.shape) # 크기 출력
-
 print('t[0] t[1] t[-1] = ', t[0], t[1], t[-1]) # Element
 print('t[2:5] t[4:-1] = ', t[2:5], t[4:-1]) # Slicing
 print('t[:2] t[3:] = ', t[:2], t[3:]) # Slicing
@@ -17,8 +21,7 @@ print()
 # 2D Array with NumPy
 t = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.], [10., 11., 12.]])
 print('2D Array with NumPy')
-print(t)
-
+print('t = ', t)
 print('Rank of t: ', t.ndim) # 차원 출력
 print('Shape of t: ', t.shape) # 크기 출력
 print('t[:, 1] = ', t[:, 1])
@@ -26,15 +29,21 @@ print('Shape of t[:, 1] = ' ,t[:, 1].shape)
 print('t[:, :-1] = \n', t[:, :-1])
 print()
 
+# 3D Array with NumPy
+t = np.array([[[1,2,3,4], [2,4,5,6]], [[3,4,5,6],[4,5,6,7]],
+             [[5,6,7,8],[6,7,8,9]]])
+print('3D Array with NumPy')
+print('t = \n', t)
+print('Rank of t: ', t.ndim) # 차원 출력
+print('Shape of t: ', t.shape) # 크기 출력
+print()
 # 1D Array with PyTorch
 t = torch.FloatTensor([0., 1., 2., 3., 4., 5., 6.])
 print('1D Array with Pytorch')
-print(t)
-
+print('t = ', t)
 print('Rank of t: ', t.dim()) # 차원 출력
 print('Shape of t: ', t.shape) # 크기 출력
 print('Size of t: ', t.size()) # 크기 출력
-
 print('t[0] t[1] t[-1] = ', t[0], t[1], t[-1]) # Element
 print('t[2:5] t[4:-1] = ', t[2:5], t[4:-1]) # Slicing
 print('t[:2] t[3:] = ', t[:2], t[3:]) # Slicing
@@ -46,8 +55,7 @@ t = torch.FloatTensor([[1., 2., 3.],
                        [7., 8., 9.],
                        [10., 11., 12.]])
 print('2D Array with Pytorch')
-print(t)
-
+print('t = ', t)
 print('Rank of t: ', t.dim()) # 차원 출력
 print('Shape of t: ', t.shape) # 크기 출력
 print('Size of t: ', t.size()) # 크기 출력
