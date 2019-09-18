c28c17b23d9417c03072d07a17b6074775a6410c
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 01:26:51 +0900

    Update Multivariable.torch.py

--- Multivariable.torch.py
+++ Multivariable.torch.py
@@ -1,5 +1,16 @@
 import torch
 import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MultivariateLinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(3, 1)
+
+    def forward(self, x):
+        return self.linear(x)
+
 # 데이터
 x_train = torch.FloatTensor([[73, 80, 75],
                              [93, 88, 93],
@@ -7,26 +18,29 @@ x_train = torch.FloatTensor([[73, 80, 75],
                              [96, 98, 100],
                              [73, 66, 70]])
 y_train = torch.FloatTensor([[152], [185], [180], [196], [142]])
+
 # 모델 초기화
-W = torch.zeros((3,1), requires_grad=True)
-b = torch.zeros(1, requires_grad=True)
+model = MultivariateLinearRegressionModel()
+
 # optimizer 설정
-optimizer = optim.SGD([W, b], lr=0.00001)
+optimizer = optim.SGD(model.parameters(), lr=0.00001)
 
 nb_epochs = 20000
 for epoch in range(1, nb_epochs + 1):
     # H(x) 계산
-    hypothesis = x_train.matmul(W) + b # or .mm or @
+    prediction = model(x_train)
+
     # cost 계산
-    cost = torch.mean((hypothesis - y_train) ** 2)
+    cost = F.mse_loss(prediction, y_train)
+
     # cost로 H(x) 개선
     optimizer.zero_grad()
     cost.backward()
     optimizer.step()
+
     # 10마다 로그 출력
     if epoch % 100 == 0:
-        print('Epoch {:4d}/{} hypothesis: {} Cost: {:.6f}'
-              .format(epoch, nb_epochs, hypothesis.squeeze().detach(),
-                      cost.item()))
+        print('Epoch {:4d}/{} Cost: {:.6f}'
+              .format(epoch, nb_epochs, cost.item()))
 
 print(y_train)
