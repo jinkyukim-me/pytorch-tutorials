8ead928bd00adb6bb832338458d41bdef446fc52
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 00:19:54 +0900

    Update Multivariable.py

--- Multivariable.py
+++ Multivariable.py
@@ -11,9 +11,9 @@ y_train = torch.FloatTensor([[152], [185], [180], [196], [142]])
 W = torch.zeros((3,1), requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
 # optimizer 설정
-optimizer = optim.SGD([w, b], lr=1-e)
+optimizer = optim.SGD([W, b], lr=0.000043)
 
-nb_epochs = 100
+nb_epochs = 13000
 for epoch in range(1, nb_epochs + 1):
     # H(x) 계산
     hypothesis = x_train.matmul(W) + b # or .mm or @
@@ -23,8 +23,10 @@ for epoch in range(1, nb_epochs + 1):
     optimizer.zero_grad()
     cost.backward()
     optimizer.step()
+    # 10마다 로그 출력
+    if epoch % 10 == 0:
+        print('Epoch {:4d}/{} hypothesis: {} Cost: {:.6f}'
+              .format(epoch, nb_epochs, hypothesis.squeeze().detach(),
+                      cost.item()))
 
-    print('Epoch {}/{} hypothesis: {} Cost: {}'
-          .format(epoch, nb_epochs, hypothesis.unsqueeze().detach(), cost.item()))
-
-
+print(y_train)
