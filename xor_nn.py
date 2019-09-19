5b9f89a701fba2b4a9bfad3b0c782915fae37550
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 23:30:14 +0900

    초기 설정

---
+++ xor_nn.py
@@ -0,0 +1,10 @@
+import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# for reproducibility
+torch.manual_seed(777)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+
+X = torch.FloatTensor([[0, 0], [0, 1], [1, 0], [1, 1]]).to(device)
+Y = torch.FloatTensor([[0], [1], [1], [0]]).to(device)
+
