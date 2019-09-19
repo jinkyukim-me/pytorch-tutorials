b66bcb4b79359566152fe2c3ad0ae239ff9521f9
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 14:35:36 +0900

    에러 수정

--- minibatch.py
+++ minibatch.py
@@ -1,23 +1,64 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-import torch.functional as F
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+
+class MultivariateLinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(3, 1)
+
+    def forward(self, x):
+        return self.linear(x)
+
 
 # Dataset
 class CustomDataset(Dataset):
     def __init__(self):
         self.x_data = [[73, 80, 75], [93, 88, 93], [89, 91, 90], [96, 98, 100], [73, 66, 70]]
         self.y_data = [[152], [185], [180], [196], [142]]
-    def len(self):
+
+    def __len__(self):
         return len(self.x_data)
+
     def __getitem__(self, idx):
         x = torch.FloatTensor(self.x_data[idx])
         y = torch.FloatTensor(self.y_data[idx])
-        return x,y
+
+        return x, y
 
 dataset = CustomDataset()
 
 # DataLoader
-dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+dataloader = DataLoader(
+    dataset,
+    batch_size=2,
+    shuffle = True
+)
+
+# 모델 초기화
+model = MultivariateLinearRegressionModel()
+
+# optimizer 설정
+optimizer = optim.SGD(model.parameters(), lr=0.000001)
+
+nb_epochs = 30
+for epoch in range(nb_epochs + 1):
+    for batch_idx, samples in enumerate(dataloader):
+        x_train, y_train = samples
+        # H(x) 계산
+        prediction = model(x_train)
+        # cost 계산
+        cost = F.mse_loss(prediction, y_train)
+
+        # cost로 H(x) 개선
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+
+        print('Epoch {:4d}/{} Batch {}/{} Cost: {:.6f}'
+              .format(epoch, nb_epochs, batch_idx + 1,
+                      len(dataloader), cost.item()
+                      ))
