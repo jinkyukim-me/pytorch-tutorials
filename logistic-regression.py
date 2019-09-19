9bee106266793bbc7e1af42969763e2e4b55ee2d
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 16:28:29 +0900

    모델의 정확도 계산 추가

--- logistic-regression.py
+++ logistic-regression.py
@@ -19,7 +19,7 @@ y_train = torch.FloatTensor(y_data)
 W = torch.zeros((2, 1), requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
 # optimizer 설정
-optimizer = optim.SGD([W, b], lr=1)
+optimizer = optim.SGD([W, b], lr=0.05)
 
 nb_epochs = 1000
 for epoch in range(nb_epochs + 1):
@@ -34,8 +34,16 @@ for epoch in range(nb_epochs + 1):
     cost.backward()
     optimizer.step()
 
-    # 100번마다 로그 출력
-    if epoch % 100 == 0:
+    # 10번마다 로그 출력
+    if epoch % 10 == 0:
         print('Epoch {:4d}/{} Cost: {:.6f}'.format(
             epoch, nb_epochs, cost.item()
         ))
+
+# 모델의 정확도 계산
+hypothesis = torch.sigmoid(x_train.matmul(W) + b)
+prediction = hypothesis >= torch.FloatTensor([0.5])
+correct_prediction = prediction.float() == y_train
+accuracy = correct_prediction.sum().item() / len(correct_prediction)
+print('The model has an accuracy of {:2.2f}% for the training set.'
+      .format(accuracy * 100))
