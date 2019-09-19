2627a54cba11d37e588e936a0027130baafd98a8
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 17:28:41 +0900

    Update logistic-resgression-higher-level.py

--- logistic-regression-higher-level.py
+++ logistic-regression-higher-level.py
@@ -1,4 +1,5 @@
 # imports
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,11 +8,50 @@ import torch.optim as optim
 # For reproducibility
 torch.manual_seed(1)
 
-# data
-x_data = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
-y_data = [[0], [0], [0], [1], [1], [1]]
+data = np.loadtxt('C:\dev\pytorch-tutorials\csv\data-03-diabetes.csv',
+                  delimiter=',', dtype=np.float32)
+# print(data)
+# print(data.shape)
+x_data = data[:, 0:-1]
+y_data = data[:, [-1]]
+x_train = torch.FloatTensor(x_data)
+y_train = torch.FloatTensor(y_data)
+# print(x_train[0:5])
+# print(y_train[0:5])
 
 class BinaryClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear = nn.Linear()
+        self.linear = nn.Linear(8, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.linear(x))
+
+model = BinaryClassifier()
+
+# optimizer 설정
+optimizer = optim.SGD(model.parameters(), lr=1)
+
+nb_epochs = 100
+for epoch in range(nb_epochs + 1):
+
+    # H(x) 계산
+    hypothesis = model(x_train)
+
+    # cost 계산
+    cost = F.binary_cross_entropy(hypothesis, y_train)
+
+    # cost로 H(x) 개선
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    # 20번마다 로그 출력
+    if epoch % 10 == 0:
+        prediction = hypothesis >= torch.FloatTensor([0.5])
+        correct_prediction = prediction.float() == y_train
+        accuracy = correct_prediction.sum().item() / len(correct_prediction)
+        print('Epoch {:4d}/{} Cost: {:.6f} Accuracy {:2.2f}%'.format(
+            epoch, nb_epochs, cost.item(), accuracy * 100,
+        ))
