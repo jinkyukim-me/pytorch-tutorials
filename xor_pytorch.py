c10936b119b1e6290b951d520566d1a28b04cfc1
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 23:05:26 +0900

    Update xor_pytorch.py

--- xor_pytorch.py
+++ xor_pytorch.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 device = torch.device("cuda:0")
-x = torch.FloatTensor([[0, 0], [0, 1], [1, 0], [1, 1]]).to(device)
-y = torch.FloatTensor([[0], [1], [1], [0]]).to(device)
+X = torch.FloatTensor([[0, 0], [0, 1], [1, 0], [1, 1]]).to(device)
+Y = torch.FloatTensor([[0], [1], [1], [0]]).to(device)
 
 # nn Layers
 linear = torch.nn.Linear(2, 1, bias=True)
@@ -13,3 +13,14 @@ sigmoid = torch.nn.Sigmoid()
 model = torch.nn.Sequential(linear, sigmoid).to(device)
 
 # define cost/loss & optimer
+criterion = torch.nn.BCELoss().to(device)
+optimizer = torch.optim.SGD(model.parameters(), lr=1)
+for step in range(10001):
+    optimizer.zero_grad()
+    hypothesis = model(X)
+    #cost/loss function
+    cost = criterion(hypothesis, Y)
+    cost.backward()
+    optimizer.step()
+    if step % 100 == 0:
+        print(step, cost.item())
