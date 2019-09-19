4288f18cc8afa7f77c6a20d09042a7c4245add77
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-19 16:16:57 +0900

    Update logistic-regression.py

--- logistic-regression.py
+++ logistic-regression.py
@@ -3,14 +3,39 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.nn as nn
+
 # For reproducibility
 torch.manual_seed(1)
-# Traing Data
+
+# 데이터
 x_data = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]] # |x_data|=(6,2) m=6 d=2
 y_data = [[0], [0], [0], [1], [1], [1]] # |y_data|=(6,1)
 x_train = torch.FloatTensor(x_data)
 y_train = torch.FloatTensor(y_data)
-print(x_train.shape)
-print(y_train.shape)
-# Computing the Hypothesis
-print('e^1 equals: ', torch.exp(torch.FloatTensor([1])))
+# print(x_train.shape)
+# print(y_train.shape)
+
+# 모델 초기화
+W = torch.zeros((2, 1), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+# optimizer 설정
+optimizer = optim.SGD([W, b], lr=1)
+
+nb_epochs = 1000
+for epoch in range(nb_epochs + 1):
+
+    # Cost 계산
+    hypothesis = torch.sigmoid(x_train.matmul(W) + b) # or .mm or @
+    cost = -(y_train * torch.log(hypothesis) +
+             (1 - y_train) * torch.log(1 - hypothesis)).mean()
+
+    # cost로 H(x) 개선
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    # 100번마다 로그 출력
+    if epoch % 100 == 0:
+        print('Epoch {:4d}/{} Cost: {:.6f}'.format(
+            epoch, nb_epochs, cost.item()
+        ))
