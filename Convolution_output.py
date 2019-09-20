51b289f5a34f50010cef8fb6de7ad90592e9d732
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-20 21:52:46 +0900

    주석 추가

--- Convolution_output.py
+++ Convolution_output.py
@@ -6,10 +6,13 @@ filter size = 11 x 11
 stride = 4
 padding = 0
 '''
+# filter size
 conv = nn.Conv2d(1, 1, 11, stride=4, padding=0)
 print('conv = ', conv)
+# input image size
 inputs = torch.Tensor(1, 1, 227, 227)
 print('inputs.shape = ', inputs.shape)
+# output image size
 out = conv(inputs)
 print('out = \n', out)
 print('out.shape = ', out.shape)
