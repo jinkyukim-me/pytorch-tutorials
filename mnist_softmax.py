8e3de0d6af44b844a985117c994054f446bc0104
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-20 12:49:59 +0900

    Update mnmist_softmax.py

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -1,6 +1,35 @@
 import torch
+import torch.utils
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
+import random
 
+# GPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# for reproducibility
+torch.manual_seed(777)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+
+# parameters
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+# mnnist dataset
+mnist_train = dsets.MNIST(root='MNIST_data/',
+                          train=True,
+                          transform=transforms.ToTensor(),
+                          download=True)
+mnist_test = dsets.MNIST(root='MNIST_data/',
+                         train=False,
+                         transform=transforms.ToTensor()
+                         download=True)
+# dataloader
+data_loader = torch.utils.data.DataLoader(dataset=mnist_train,
+                                          batch_size=batch_size,
+                                          shuffle=True,
+                                          drop_last=True)
