7ed3596a23765e13f92ffefdc4e27284907aff6a
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-20 21:50:47 +0900

    Update Convolution_output.py

--- Convolution_output.py
+++ Convolution_output.py
@@ -9,4 +9,7 @@ padding = 0
 conv = nn.Conv2d(1, 1, 11, stride=4, padding=0)
 print('conv = ', conv)
 inputs = torch.Tensor(1, 1, 227, 227)
-print(inputs.shape)
+print('inputs.shape = ', inputs.shape)
+out = conv(inputs)
+print('out = \n', out)
+print('out.shape = ', out.shape)
