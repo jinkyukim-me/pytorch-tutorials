bbb1715407ffdffe7b0d64efcbf61922e83e2ed5
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-20 13:15:48 +0900

    초기 설정값 설정

---
+++ mnist_nn.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import random
+
+# GPU
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# for reproducibility
+torch.manual_seed(777)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+
+# parameters
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+# mnnist dataset
+mnist_train = dsets.MNIST(root='MNIST_data/',
+                          train=True,
+                          transform=transforms.ToTensor(),
+                          download=True)
+mnist_test = dsets.MNIST(root='MNIST_data/',
+                         train=False,
+                         transform=transforms.ToTensor(),
+                         download=True)
+# dataloader
+data_loader = torch.utils.data.DataLoader(dataset=mnist_train,
+                                          batch_size=batch_size,
+                                          shuffle=True,
+                                          drop_last=True)
