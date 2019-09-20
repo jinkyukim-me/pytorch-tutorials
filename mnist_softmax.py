240498f2bd8330d45cfe6ea49b5e83a4f5274543
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-20 13:08:49 +0900

    코드 정리

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -1,7 +1,6 @@
 import torch
-import torch.utils
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.optim as optim
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import random
@@ -26,10 +25,62 @@ mnist_train = dsets.MNIST(root='MNIST_data/',
                           download=True)
 mnist_test = dsets.MNIST(root='MNIST_data/',
                          train=False,
-                         transform=transforms.ToTensor()
+                         transform=transforms.ToTensor(),
                          download=True)
 # dataloader
 data_loader = torch.utils.data.DataLoader(dataset=mnist_train,
                                           batch_size=batch_size,
                                           shuffle=True,
                                           drop_last=True)
+
+# MNIST data image of shape 28 * 28 = 784
+linear = nn.Linear(784, 10, bias=True).to(device)
+
+# Initialization
+nn.init.normal_(linear.weight)
+
+# define cost/loss & optimizer
+criterion = nn.CrossEntropyLoss().to(device)    # Softmax is internally computed.
+optimizer = optim.Adam(linear.parameters(), lr=learning_rate)
+
+# train
+total_batch = len(data_loader)
+for epoch in range(training_epochs):
+    avg_cost = 0
+
+    for X, Y in data_loader:
+        # reshape input image into [batch_size by 784]
+        # label is not one-hot encoded
+        X = X.view(-1, 28 * 28).to(device)
+        Y = Y.to(device)
+
+        optimizer.zero_grad()
+        hypothesis = linear(X)
+        cost = criterion(hypothesis, Y)
+        cost.backward()
+        optimizer.step()
+
+        avg_cost += cost / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning finished')
+
+# Test the model using test sets
+with torch.no_grad():
+    X_test = mnist_test.test_data.view(-1, 28 * 28).float().to(device)
+    Y_test = mnist_test.test_labels.to(device)
+
+    prediction = linear(X_test)
+    correct_prediction = torch.argmax(prediction, 1) == Y_test
+    accuracy = correct_prediction.float().mean()
+    print('Accuracy:', accuracy.item())
+
+    # Get one and predict
+    r = random.randint(0, len(mnist_test) - 1)
+    X_single_data = mnist_test.test_data[r:r + 1].view(-1, 28 * 28).float().to(device)
+    Y_single_data = mnist_test.test_labels[r:r + 1].to(device)
+
+    print('Label: ', Y_single_data.item())
+    single_prediction = linear(X_single_data)
+    print('Prediction: ', torch.argmax(single_prediction, 1).item())
