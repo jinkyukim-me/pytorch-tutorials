0c82221dbd4f78d8e3dd65327b5bfe84bdf624d8
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-21 11:45:34 +0900

    Update tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -1,1 +1,5 @@
-import tokenization
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+print(word_tokenize("Don't ne fooled by the dark sounding name,"
+                    "Mr. Jone's Orphanage is as cheery goes for a pastry shop"))
